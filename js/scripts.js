6e390fdaf15f811e9456d482aa90d99ec0b95bf3
Julio Sanquis <jsanquis@gmail.com>
2014-09-10 21:18:54 +0200

    Cambios de ejemplos

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,8 @@
 var variableGlobal = 10;
 
 // Creamos una función
-miFuncion = function(par, pare){
-	
-	var variableLocal = 20;
-	var suma = variableGlobal + variableLocal + par;
-	var resultado = pare + suma;
-
-    document.write(resultado + "<br />");
-
-    console.log("variableGlobal" + " + " + "variableLocal");
+function textoBody() {
+	document.write("Si funciona mi JavaScript");
 };
 
-miFuncion(10, "La suma de las variables es igual a: ");
-miFuncion(100, "Las variables suman: ");
-miFuncion(150, "La suma es igual a: ");
+textoBody();
